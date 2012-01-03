ee9f50a9704a1abeb604ada66ccbb9e50fea7e58
Yann Rochard <yann.rochard@gmail.com>
2012-01-03 17:53:08 +0100

    Ajout d'une nouvelle route.

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ BrowserComparator::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'browser_comparator#index'
 
   # See how all your routes lay out with "rake routes"
 
