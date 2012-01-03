6110d797e158046cf370a50ac9babf8c8f0efb32
JB Amphoux <jb.amphoux@gmail.com>
2012-01-03 16:06:18 +0100

    Using seeds to populate browers in database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+BrowserFamily.find_or_create_by_name(:name => "Firefox", :prod_society => "Mozilla")
+BrowserFamily.find_or_create_by_name(:name => "Google Chrome", :prod_society => "Google")
+BrowserFamily.find_or_create_by_name(:name => "Internet Explorer", :prod_society => "Microsoft")
+BrowserFamily.find_or_create_by_name(:name => "Safari", :prod_society => "Apple")
+BrowserFamily.find_or_create_by_name(:name => "Opera", :prod_society => "Opera Software")
