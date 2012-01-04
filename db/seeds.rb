7ef34675a974c016c5e924287d11a52e07728141
Yann Rochard <yann.rochard@gmail.com>
2012-01-04 18:15:25 +0100

    Modification des seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,7 +10,7 @@
 
 
 browser_families_attributes = [
-  {name: "Firefox", prod_society: "Mozilla"}, 
+  {name: "Firefox", prod_society: "Mozilla"},
   {name: "Google Chrome", prod_society: "Google"},
   {name: "Internet Explorer", prod_society: "Microsoft"},
   {name: "Safari", prod_society: "Apple"},
@@ -24,5 +26,19 @@ category_names.each do |name|
   Category.find_or_create_by_name(name: name)
 end
 
+def browser_id(name)
+  browser = BrowserFamily.find_by_name(name)
+  browser.id
+end
 
+version_attributes = [
+  {name: "v2", date_opening: "2010/01/06", description: "Pour Mac OS X", browser_family_id: browser_id("Safari")},
+  {name: "v3", date_opening: "2010/11/08", description: "Pour Mac OS X + Antiphishing", browser_family_id: browser_id("Safari")},
+  {name: "v7", date_opening: "2010/10/06", description: "Pour Active X control", browser_family_id: browser_id("Internet Explorer")},
+  {name: "v0.2.149", date_opening: "2002/09/08", description: "Premiere version", browser_family_id: browser_id("Google Chrome")},
+  {name: "v3", date_opening: "2001/06/08", description: "8 million DL", browser_family_id: browser_id("Firefox")},
+  {name: "v9.1", date_opening: "2001/01/06", description: "GeoTrust, phishTank", browser_family_id: browser_id("Opera")}]
 
+version_attributes.each do |attributes|
+  Version.create(attributes)
+end
