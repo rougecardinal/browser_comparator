6b081bd49c7e8f46d14bbd1e82642e9644b977bc
Yann Rochard <yann.rochard@gmail.com>
2012-01-05 18:11:56 +0100

    Ajout d'une 2ème version pour chaque browser.

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,36 +20,17 @@ browser_families_attributes.each do |attributes|
   BrowserFamily.find_or_create_by_name(attributes)
 end
 
-
 category_names = ["Aide/Support", "Securite", "Configuration", "Divers" ]
 category_names.each do |name|
   Category.find_or_create_by_name(name: name)
 end
 
-# def browser_id(name)
-#   browser = BrowserFamily.find_by_name(name)
-#   browser.id
-# end
-#
-# version_attributes_old = [
-#   {name: "v2", date_opening: "2010/01/06", description: "Pour Mac OS X", browser_family_id: browser_id("Safari")},
-#   {name: "v3", date_opening: "2010/11/08", description: "Pour Mac OS X + Antiphishing", browser_family_id: browser_id("Safari")},
-#   {name: "v7", date_opening: "2010/10/06", description: "Pour Active X control", browser_family_id: browser_id("Internet Explorer")},
-#   {name: "v0.2.149", date_opening: "2002/09/08", description: "Premiere version", browser_family_id: browser_id("Google Chrome")},
-#   {name: "v3", date_opening: "2001/06/08", description: "8 million DL", browser_family_id: browser_id("Firefox")},
-#   {name: "v9.1", date_opening: "2001/01/06", description: "GeoTrust, phishTank", browser_family_id: browser_id("Opera")}]
-#
-# version_attributes_old.each do |attributes|
-#   Version.c reate(attributes)
-# end
-
-
 browsers_versions = {
-  "Safari" => [ { name: "v5.1.2", date_opening: "2009/11/29", description: "Dernière version Mac OS X"}],
-  "Firefox" => [ { name: "v9.0.1", date_opening: "2011/12/11", description: "Dernière version Firefox"} ],
-  "Google Chrome" => [ { name: "v16.0.912.63", date_opening: "2011/12/13", description: "Dernière version Google Chrome."} ],
-  "Internet Explorer" => [ { name: "v9.0.4", date_opening: "2011/12/22", description: "Dernière version Internet Explorer."} ],
-  "Opera" => [ { name: "v11.6", date_opening: "2011/12/06", description: "Dernière version Opera."} ]
+  "Safari" => [{name: "v5.1.2", date_opening: "2009/11/29", description: "Dernière version Mac OS X"}, {name: "v5.1.1", date_opening: "2011/10/12", description: "Avant dernière version Mac OS X"}],
+  "Firefox" => [{name: "v9.0.1", date_opening: "2011/12/11", description: "Dernière version Firefox"}, {name: "v8.0", date_opening: "2011/11/08", description: "Avant dernière version Firefox"}],
+  "Google Chrome" => [{name: "v16.0.912.63", date_opening: "2011/12/13", description: "Dernière version Google Chrome."}, {name: "v15.0.874", date_opening: "2011/10/27", description: "Avant dernière version Google Chrome."}],
+  "Internet Explorer" => [{name: "v9.0.4", date_opening: "2011/12/22", description: "Dernière version Internet Explorer."}, {name: "v8.0", date_opening: "2009/03/19", description: "Avant dernière version Internet Explorer."}],
+  "Opera" => [{name: "v11.60", date_opening: "2011/12/06", description: "Dernière version Opera."}, {name: "v11.50", date_opening: "2011/06/28", description: "Avant dernière version Opera."}]
 }
 
 browsers_versions.each do |browser_name, versions_attributes|
