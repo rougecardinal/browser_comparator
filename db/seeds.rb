f4c0cdd519c78efec524e12c4d258e58125159cc
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-01-09 12:43:01 +0100

    Commentaires de fonctions et de lignes de code. Suppression de lignes inutiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # BROWSERS.
+# Tableau de hashs avec les attributs de BrowserFamily
 browser_families_attributes = [
   {name: "Firefox", prod_society: "Mozilla"},
   {name: "Google Chrome", prod_society: "Google"},
@@ -16,6 +17,8 @@ browser_families_attributes = [
   {name: "Safari", prod_society: "Apple"},
   {name: "Opera", prod_society: "Opera Software"}
 ]
+
+# Parcours du tableau et création du browser s'il n'existe pas
 browser_families_attributes.each do |attributes|
   BrowserFamily.find_or_create_by_name(attributes)
 end
@@ -26,6 +29,7 @@ category_names.each do |name|
 end
 
 #VERSIONS.
+#Tableau de hashs.
 browsers_versions = {
   "Safari" => [{name: "5", date_opening: " 2009/11/29", description: "Dernière version Mac OS X"}, {name: "4", date_opening: "2011/10/12", description: "Avant dernière version Mac OS X"}],
   "Firefox" => [{name: "9", date_opening: "2011/12/11", description: "Dernière version Firefox"}, {name: "8", date_opening: "2011/11/08", description: "Avant dernière version Firefox"}],
@@ -34,48 +38,43 @@ browsers_versions = {
   "Opera" => [{name: "11", date_opening: "2011/12/06", description: "Dernière version Opera."}, {name: "10", date_opening: "2011/06/28", description: "Avant dernière version Opera."}]
 }
 
+#Parcours du hash avec les deux attributs Browser_name et Versions_attributes.
+#Recherche du browser par son nom.
 browsers_versions.each do |browser_name, versions_attributes|
   browser = BrowserFamily.find_by_name(browser_name)
   versions_attributes.each do |version_attributes|
-    version_attributes.merge!(browser_family_id: browser.id)
+    version_attributes.merge!(browser_family_id: browser.id) #Merge! => Ajout du hash passé en paramètre au hash précédent.
     Version.find_or_create_by_name_and_browser_family_id(version_attributes)
   end
 end
 
 #OS.
-os_names = [ "Windows 7", "Windows Vista", "Windows XP", "Mac OS Lion", "Mac OS Snow Leopard", "Mac OS Leopard", "Linux Ubuntu", "Linux Autres"]
-    
+os_names = [ 
+  "Windows 7", "Windows Vista", "Windows XP",
+  "Mac OS Lion", "Mac OS Snow Leopard", "Mac OS Leopard",
+  "Linux Ubuntu", "Linux Autres"]    
 os_names.each do |attribute|
   Os.find_or_create_by_name(attribute)
 end
 
 #OS VERSION.
-
-puts "Association des versions et des os"
-
-linux = Os.find_by_name("Linux Ubuntu")
-
-#versions_in_browsers = {"Google Chrome" => ["v16","v15"], "Safari" => ["v5","v4"]}
-
 versions_in_browsers = {
   "Windows 7" => {
-      "Google Chrome" => ["16", "15"],
-      "Safari" => ["5", "4"]
+    "Google Chrome" => ["16", "15"], "Safari" => ["5", "4"]
   },
   "Mac OS Lion" => {
-    "Firefox" => ["16", "15"],
-    "Safari" => ["5", "4"]
+    "Firefox" => ["16", "15"], "Safari" => ["5", "4"]
   }
 }
 
+#La fonction retourne les browsers et leurs versions en fonction de l'OS 
 versions_in_browsers.each do |os, values|
   #puts " - key : #{os}, values : #{values}"
   values.each do |browser, versions|
-    #puts "-- key #{browser}, val : #{versions}"
+    #puts "-- key #{browser}, val : #{versions}" 
     versions.each do |version|
       #puts "--- Os :#{os} => Browser : #{browser} => Version : #{version}"
-      OsVersion.create(version_id: Version.find_by_name(version).id, os_id: Os.find_by_name(os).id  )  
+      OsVersion.find_or_create_by_version_id(version_id: Version.find_by_name(version).id, os_id: Os.find_by_name(os).id  )  
     end
   end    
 end
-
