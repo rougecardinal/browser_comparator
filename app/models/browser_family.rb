02fb40b5b6bb7d874380d63cfd4d52b1151ea35b
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-01-10 17:48:00 +0100

    Création d'une fonction permettant d'afficher l'id des Features Versions en fonction d'un browser

--- app/models/browser_family.rb
+++ app/models/browser_family.rb
@@ -1,3 +1,7 @@
 class BrowserFamily < ActiveRecord::Base
   has_many :versions
+  
+  def last_version_has_feature?(feature)
+    versions.last.feature_versions.where(feature_id: feature.id).first != nil
+  end
 end
