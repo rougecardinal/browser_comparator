75b6bb8954431420e4e59d290a9e95c5e16845b0
Yann Rochard <yann.rochard@gmail.com>
2012-01-16 18:31:03 +0100

    Recherche pour savoir quel est le current browser

--- app/controllers/browser_comparator_controller.rb
+++ app/controllers/browser_comparator_controller.rb
@@ -1,8 +1,11 @@
 class BrowserComparatorController < ApplicationController
 
+
   def index
     @browsers = BrowserFamily.all
     @features = Feature.all
+    @current_browser = request.env['HTTP_USER_AGENT']
   end
+
 
 end
