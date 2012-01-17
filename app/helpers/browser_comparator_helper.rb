e7b14b8adba426c1c6a8dad3107f1461b77e544a
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-01-17 15:04:41 +0100

    Ajout d'une fonction dans le helper qui compare les valeurs du current browser et des autres browsers

--- app/helpers/browser_comparator_helper.rb
+++ app/helpers/browser_comparator_helper.rb
@@ -1,2 +1,7 @@
 module BrowserComparatorHelper
+  
+  def compare_features_value(browser, feature)
+    browser.last_version_has_feature?(feature) == @current_browser.last_version_has_feature?(feature)
+  end
+  
 end
