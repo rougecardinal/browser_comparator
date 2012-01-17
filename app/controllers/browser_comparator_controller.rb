783167455c972b5f1df1ae5edf35e616a88fe1cb
Yann Rochard <yann.rochard@gmail.com>
2012-01-17 10:36:09 +0100

    Modification méthode current_agent pour utiliser la classe BrowserFamily et donc récupérer un browser de la base de donnée ainsi que toutes ses infos (versions, features etc)

--- app/controllers/browser_comparator_controller.rb
+++ app/controllers/browser_comparator_controller.rb
@@ -3,12 +3,17 @@ class BrowserComparatorController < ApplicationController
   def index
     @browsers = BrowserFamily.all
     @features = Feature.all
-    current_agent 
+    current_user_agent 
     # Appel de la methode current_agent dans la methode index 
     # pour pouvoir l'utiliser dans la vue index.html.haml
   end
   
-  def current_agent
-    @current_browser = Agent.new request.env['HTTP_USER_AGENT']
+  def current_user_agent
+    current_agent = Agent.new request.env['HTTP_USER_AGENT']    
+    current_agent_version_name = current_agent.version.split(".")[0]
+    
+    @current_browser = BrowserFamily.find_by_name(current_agent.name)
+    @current_browser_version = Version.find_by_name_and_browser_family_id(current_agent_version_name, @current_browser.id)
+    
   end
 end
