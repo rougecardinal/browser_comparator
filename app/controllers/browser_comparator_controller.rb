1a987ca6a22e15e105c251a578b3f5bf03078b50
Yann Rochard <yann.rochard@gmail.com>
2012-01-17 09:50:40 +0100

    Création methode current_agent qui renvoie les informations du current browser.

--- app/controllers/browser_comparator_controller.rb
+++ app/controllers/browser_comparator_controller.rb
@@ -3,11 +3,12 @@ class BrowserComparatorController < ApplicationController
   def index
     @browsers = BrowserFamily.all
     @features = Feature.all
-    current_agent
+    current_agent 
+    # Appel de la methode current_agent dans la methode index 
+    # pour pouvoir l'utiliser dans la vue index.html.haml
   end
   
   def current_agent
-    informations = request.env['HTTP_USER_AGENT']
-    @current_browser = Agent.new "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7"
+    @current_browser = Agent.new request.env['HTTP_USER_AGENT']
   end
 end
