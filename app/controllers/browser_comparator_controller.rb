a9ad56587ae15a9fe65a555f0b0c8cf88232c9a0
Yann Rochard <yann.rochard@gmail.com>
2012-01-17 09:34:33 +0100

    Première utilsiatin de la gemme dans un contrôler avec un user agent static.

--- app/controllers/browser_comparator_controller.rb
+++ app/controllers/browser_comparator_controller.rb
@@ -1,11 +1,13 @@
 class BrowserComparatorController < ApplicationController
 
-
   def index
     @browsers = BrowserFamily.all
     @features = Feature.all
-    @current_browser = request.env['HTTP_USER_AGENT']
+    current_agent
   end
-
-
+  
+  def current_agent
+    informations = request.env['HTTP_USER_AGENT']
+    @current_browser = Agent.new "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7"
+  end
 end
