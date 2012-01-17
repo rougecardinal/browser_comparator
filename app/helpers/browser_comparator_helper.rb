8c43ad8920c699ee680d6c7fbf82b1f33516c2bd
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-01-17 18:37:29 +0100

    refactoring de la méthode de comparaison des valeurs de feutre entre deux versions de browser

--- app/helpers/browser_comparator_helper.rb
+++ app/helpers/browser_comparator_helper.rb
@@ -1,7 +1,15 @@
 module BrowserComparatorHelper
   
   def compare_features_value(browser, feature)
-    browser.last_version_has_feature?(feature) == @current_browser.last_version_has_feature?(feature)
+    css_class = ""
+    if browser.last_version_has_feature?(feature)
+      text = "Oui"
+      css_class  = "has_not_and_has" if !@current_browser.last_version_has_feature?(feature)
+    else
+      text = "Non"
+      css_class  = "has_and_has_not" if @current_browser.last_version_has_feature?(feature)
+    end
+
+    content_tag(:td, text, class: css_class)
   end
-  
 end
