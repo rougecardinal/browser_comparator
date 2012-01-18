2e14f32bcb7b33cc4f7dbd6f52fc8ed7a9b6165d
Yann Rochard <yann.rochard@gmail.com>
2012-01-18 10:34:39 +0100

    Mise en place d'une classe CSS sur le tr en fonction du current browser et de sa feutre. (has_feature // has_not_feature)

--- app/helpers/browser_comparator_helper.rb
+++ app/helpers/browser_comparator_helper.rb
@@ -1,15 +1,21 @@
 module BrowserComparatorHelper
-  
+
   def compare_features_value(browser, feature)
-    css_class = ""
     if browser.last_version_has_feature?(feature)
       text = "Oui"
-      css_class  = "has_not_and_has" if !@current_browser.last_version_has_feature?(feature)
+      css_class  = "has"
     else
       text = "Non"
-      css_class  = "has_and_has_not" if @current_browser.last_version_has_feature?(feature)
+      css_class  = "has_not"
     end
-
     content_tag(:td, text, class: css_class)
   end
+  
+  def current_browser_has_feature(feature)
+    if @current_browser.last_version_has_feature?(feature)
+      "has_feature"
+    else
+      "has_not_feature"
+    end
+  end  
 end
