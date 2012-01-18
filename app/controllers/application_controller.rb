731863a895bcdc81cbbe2fb3ca85a7728c955936
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-01-18 18:48:22 +0100

    Création d'une action change_locale qui permet la récupération du GET et de modifier la variable I18n locale Puis redirection vers la racine du site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale
 
+  def change_locale
+    I18n.locale = params[:locale_name]
+    redirect_to root_path
+  end
+
+
   private
   def set_locale
     I18n.locale = params[:locale] if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
