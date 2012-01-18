ab369161f66fdc8a70e7765082f86cfc77b3757f
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-01-18 17:34:30 +0100

    Ajout d'une nouvelle langue: FR L'application ne peut avoir que les langues qu'elle possède, grâce a la fonction set_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+  end
 end
