99b1963509afb31bc9374951f4cdfdf2dc54cd96
Yann Rochard <yann.rochard@gmail.com>
2012-01-19 09:54:59 +0100

    Ajout des clés has_feature / has_not_feature, afin d'internationaliser le tableau

--- app/helpers/browser_comparator_helper.rb
+++ app/helpers/browser_comparator_helper.rb
@@ -1,18 +1,18 @@
 module BrowserComparatorHelper
 
   def compare_features_value(version, feature, for_current_browser)
-    if version.has_feature?(feature)
-      text = "Oui"
+    if version.has_feature? feature 
+      text = t 'has_feature'
       css_class  = ["has"]
     else
-      text = "Non"
+      text = t 'has_not_feature'
       css_class  = ["has_not"]
     end
     css_class << "current_browser" if for_current_browser
     content_tag(:td, text, class: css_class)
   end
-  
+
   def current_browser_has_feature(feature)
     @last_versions[0].has_feature?(feature) ? "has_feature" : "has_not_feature"
-  end  
+  end
 end
