023160ee404ab1bdce5afadf5f86dbd44a643ea3
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-02-07 16:43:11 +0100

    Ajout d'une dépendance ( Si le browser est détruit, les versions aussi)

--- app/models/browser_family.rb
+++ app/models/browser_family.rb
@@ -1,5 +1,5 @@
 class BrowserFamily < ActiveRecord::Base
-  has_many :versions
+  has_many :versions, dependent: :destroy
   
   def last_version_has_feature?(feature)
     versions.last.has_feature? feature
