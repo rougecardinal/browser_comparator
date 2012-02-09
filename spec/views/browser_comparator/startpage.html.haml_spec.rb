be204faf94fa7151857ab3eace6126351afcf2eb
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-02-09 15:34:39 +0100

    Entrainement test sur StartPage

--- spec/views/browser_comparator/startpage.html.haml_spec.rb
+++ spec/views/browser_comparator/startpage.html.haml_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe 'browser_comparator/startpage.html.haml' do
-  it "infers the controller path" do
-    controller.request.path_parameters["controller"].should eq("browser_comparator")
-  end
-  it "infers the controller path" do
-    controller.request.path_parameters["action"].should eq("startpage")
-  end
   it "Page include string 'hahaha'" do
   	render :template => "browser_comparator/startpage.html.haml"
-  	rendered.should include("hahaha")
+  	rendered.should have_content("hahaha")
 	end
 	it " Page should have selector '#test'" do
 		render
 		rendered.should have_selector("#test")
 	end
-	
+  it "test selector" do
+    render 
+    rendered.should have_selector("td", :count => 4)
+  end
+  it 'test link' do
+    render
+    rendered.should have_content(:a)
+  end
 end
