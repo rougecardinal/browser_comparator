cdff5a8155308257709cd2eb4a6e5f142689e7a4
Yann Rochard <yann.rochard@gmail.com>
2012-02-13 18:08:09 +0100

    Simplification des liens des methodes visite

--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
 
 describe 'Index' do
-  
+
   before(:each) do
     @session = Capybara::Session.new(:selenium)
-    @session.visit 'http://browser_comparator.dev/index/'
+    Capybara.app_host = 'http://browser_comparator.dev'
+    @session.visit "/index"
   end
-  
+
   it 'current_browser is Firefox' do
     @session.should have_selector('.current_browser', :text => "Firefox")
   end
-  
+
   it 'other browsers' do
     @session.should have_selector('.other_browser', :text => "Firefox")
     @session.should have_selector('.other_browser', :text => "Opera")
     @session.should have_selector('.other_browser', :text => "Safari")
     @session.should have_selector('.other_browser', :text => "Chrome")
     @session.should have_selector('.other_browser', :text => "Internet Explorer")
+
+    @session.should have_css('th.other_browser', :count => 5)
   end
 end
--- spec/requests/startpage_spec.rb
+++ spec/requests/startpage_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
- 
+
 describe 'StartPage' do
-  
+
   before(:each) do
     @session = Capybara::Session.new(:selenium)
+    Capybara.app_host = "http://browser_comparator.dev"
   end
-  
+
   it 'is visiting by user' do
-    @session.visit 'http://browser_comparator.dev/'
+    @session.visit '/'
     @session.has_link?('See more').should be_true
   end
 end
