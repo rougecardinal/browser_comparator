bdb26f29833e06f234f922ed7a87bfbeea449177
Yann Rochard <yann.rochard@gmail.com>
2012-02-13 11:46:57 +0100

    Refactoring de la method current_user_agent

--- app/controllers/browser_comparator_controller.rb
+++ app/controllers/browser_comparator_controller.rb
@@ -16,11 +16,18 @@ class BrowserComparatorController < ApplicationController
 
   def current_user_agent
     current_agent = Agent.new request.env['HTTP_USER_AGENT']
-    #@current_agent = Agent.new request.env['HTTP_USER_AGENT']
-    current_agent_version_name = current_agent.version.split(".")[0]
-    
-    current_browser = BrowserFamily.find_by_name(current_agent.name)
-    @last_versions.unshift current_browser.versions.find_by_name(current_agent_version_name)
-    
+    str_user_agent = request.env['HTTP_USER_AGENT']
+
+    if str_user_agent.include?("Firefox")
+      user_agent = str_user_agent.split(" ")[9]
+      user_agent_name = user_agent.split("/")[0]
+      user_agent_version = user_agent.split("/")[1].split(".")[0]
+      current_browser = BrowserFamily.find_by_name(user_agent_name)
+      @last_versions.unshift current_browser.versions.find_by_name(user_agent_version)
+    else
+      current_agent_version_name = current_agent.version.split(".")[0]
+      current_browser = BrowserFamily.find_by_name(current_agent.name)
+      @last_versions.unshift current_browser.versions.find_by_name(current_agent_version_name)
+    end
   end
 end
