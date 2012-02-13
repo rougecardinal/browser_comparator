23b9ba3fb234fe525927d0cffe21ad97b7ca5a2d
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-02-13 16:52:33 +0100

    Ajout de tests Capybara sur la page index

---
+++ spec/requests/index_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'Index' do
+  
+  before(:each) do
+    @session = Capybara::Session.new(:selenium)
+    @session.visit 'http://browser_comparator.dev/index/'
+  end
+  
+  it 'current_browser is Firefox' do
+    @session.should have_selector('.current_browser', :text => "Firefox")
+  end
+  
+  it 'other browsers' do
+    @session.should have_selector('.other_browser', :text => "Firefox")
+    @session.should have_selector('.other_browser', :text => "Opera")
+    @session.should have_selector('.other_browser', :text => "Safari")
+    @session.should have_selector('.other_browser', :text => "Chrome")
+    @session.should have_selector('.other_browser', :text => "Internet Explorer")
+  end
+end
