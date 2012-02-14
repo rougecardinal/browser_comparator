d962252aca633353c8422e111ca02fd0e7532324
JeanBaptiste Amphoux <jb.amphoux@gmail.com>
2012-02-14 10:03:21 +0100

    Ajout de tests dans la vue Index

--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe 'Index' do
-
-  nb_browser = BrowserFamily.all.count
-
+  nb_browser = BrowserFamily.count
+  nb_feature = Feature.count
+  nb_category = Category.count
+  
+  
   before(:each) do
     @session = Capybara::Session.new(:selenium)
     Capybara.app_host = 'http://browser_comparator.dev'
@@ -22,5 +24,13 @@ describe 'Index' do
     @session.should have_selector('.other_browser', :text => "Internet Explorer")
 
     @session.should have_css('th.other_browser', :count => nb_browser)
+  end
+  
+  it 'has 25 features' do
+    @session.should have_css('th.feature_name', :count => nb_feature + 2)
+  end
+  
+  it 'has 3 category' do
+    @session.should have_css('th.category_name', :count => nb_category)
   end
 end
