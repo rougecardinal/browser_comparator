810ab75c187210e9a2b2348ada26132a5b0f2681
Yann Rochard <yann.rochard@gmail.com>
2012-02-25 08:44:15 +0100

    Ajout d'une méthode pour les images des browsers

--- app/helpers/browser_comparator_helper.rb
+++ app/helpers/browser_comparator_helper.rb
@@ -15,12 +15,20 @@ module BrowserComparatorHelper
   def current_browser_has_feature(feature)
     @last_versions[0].has_feature?(feature) ? "has_feature" : "has_not_feature"
   end
-  
+
   def category_name(category)
     name_to_css category.name
   end
-  
+
   def name_to_css(name)
     name.downcase.gsub("/", "_")
+  end
+
+  def browser_image(name)
+    if name == "Internet Explorer"
+      image_tag("Internet_Explorer_color.png")
+    else
+      image_tag("#{name}_color.png")
+    end
   end
 end
